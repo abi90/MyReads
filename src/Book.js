68cfacebef42c7d104e006a7f01ee6ff28de3c0f
abi90 <abisai.ramos@upr.edu>
2017-08-06 01:45:21 -0400

    fix: Fix the no thumbnail crash

--- src/Book.js
+++ src/Book.js
@@ -11,7 +11,7 @@ class Book extends Component {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={(event)=>{this.updateBook(event.target.value)}}>
               <option value="none" disabled>Move to...</option>
